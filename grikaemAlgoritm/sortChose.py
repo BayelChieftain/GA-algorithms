15a6a25e4aae9797f90f8b198ce75328144c0ccb
Bayel Chief <marvel.vovo@gmail.com>
2023-09-26 23:05:55 +0600

    fix recursion.py

---
+++ grikaemAlgoritm/sortChose.py
@@ -0,0 +1,17 @@
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+sortArr = selectionSort([23, 1, 434, 12, 34, 3444, 4, 6, 2, 9, 76, 546, 999])
+print(sortArr)
